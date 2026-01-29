fc4cfeced62ac3a9c5155413a41abb5c8f642ee1
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-01-29 13:31:36 +0300

    Add comments for doxygen

--- memory/IMemory.h
+++ memory/IMemory.h
@@ -3,13 +3,26 @@
 
 class IMemory {
 public:
-    virtual bool read(uint32_t address, uint8_t* data, size_t) = 0;
-    virtual bool write(uint32_t address, const uint8_t* data, size_t size) = 0;
+    /**
+     * @brief Чтение массива байт
+     */
+    virtual bool read(uint32_t address, uint8_t *data, size_t) = 0;
 
-    virtual bool readByte(uint32_t address, uint8_t& value) = 0;
+    /**
+    * @brief Запись массива байт
+    */
+    virtual bool write(uint32_t address, const uint8_t *data, size_t size) = 0;
+
+    /**
+    * @brief Чтение 1 байта
+    */
+    virtual bool readByte(uint32_t address, uint8_t &value) = 0;
+
+    /**
+    * @brief Чтение 1 байта
+    */
     virtual bool writeByte(uint32_t address, uint8_t value) = 0;
 
 
     virtual ~IMemory() = default;
 };
-
