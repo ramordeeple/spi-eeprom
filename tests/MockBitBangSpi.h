6bef6d9457acb17e7e78516b8dd7773823d97c94
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-01-30 13:01:26 +0300

    Add actions for logs

--- tests/MockBitBangSpi.h
+++ tests/MockBitBangSpi.h
@@ -14,26 +14,34 @@ using namespace std;
 class MockBitBangSpi : public ISpiDriver {
 public:
     /** @brief Имитирует выбор устройства на шине SPI*/
-    void select() override;
+    void select() override {
+        log.push_back(0xAA);
+    }
 
     /** @brief Имитирует снятие выбора устройства на шине SPI */
-    void deselect() override;
+    void deselect() override {
+        log.push_back(0xBB);
+    }
 
     /** @brief Имитирует отправку/приём байта по SPI */
     uint8_t transfer(uint8_t data) override {
-        (void) data;
+        log.push_back(data);
 
         return 0xFF;
     }
 
     /** @brief Очистка лога SPI операций */
-    void clearLog();
+    void clearLog() {
+        log.clear();
+    }
 
     /**
      * @brief Получение лога SPI операций
      * @return Константная ссылка на вектор логов
      */
-    const vector<uint8_t>& getLog() const;
+    const vector<uint8_t>& getLog() const {
+        return log;
+    }
 
 private:
     vector<uint8_t> log; // Лог всех SPI операций
